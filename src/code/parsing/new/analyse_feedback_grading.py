7dafcd387dc77b02b4dd2a4207be8854c483d835
Artem <artem.kucheravy@gmail.com>
2026-05-18 12:28:52 +0300

    Add students feedback evaluation and its analysis

--- src/code/parsing/new/analyse_feedback_grading.py
+++ src/code/parsing/new/analyse_feedback_grading.py
@@ -1,18 +1,26 @@
 import pandas as pd
 from src.data.constants import BASE_PATH
 import os
+from statistics import mean
 
+STUDENTS_PATH = os.path.join(BASE_PATH, "src", "results", "human", "students")
 
-FILE_PATH = os.path.join(BASE_PATH, "src", "results",   "llm", "actionable_feedback_01", "gemma4_negative_feedback_analysis", "responses", "feedback_eval_vithod.csv")
+def get_average(author : str):
+   
+    df = pd.read_csv(os.path.join(STUDENTS_PATH, f"feedback_eval_{author}.csv"))
+    grades = df.drop(columns=['ID'])
+    #print(grades)
+    #print()
+    #print(grades.stack())
+    average_score = grades.stack().mean()
 
-df = pd.read_csv(FILE_PATH)
-grades = df.drop(columns=['ID'])
-
-print(grades)
-print()
-print(grades.stack())
-# Compute the average of all grades, ignoring NaN values
-average_score = grades.stack().mean()
+    print(f"{author}: {average_score:.4f}")
+    return average_score
 
 # python -m src.code.parsing.new.analyse_feedback_grading
-print(f"Average score: {average_score:.4f}")
+grade1 = get_average("student1")
+grade2 = get_average("student2")
+grade3 = get_average("student3")
+grade4 = get_average("student4")
+
+print(f"Total average = {mean([grade1, grade2, grade3, grade4])}")
